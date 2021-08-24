9250510a52780ab9926209201ece591455c96bd2
josephadeabah <joseph.adeabah@amalitech.org>
2021-08-24 11:56:16 +0000

    intial commit

---
+++ src/features/cards/Card.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectCards } from "./cardsSlice";
+import { Link, useParams } from "react-router-dom";
+
+export default function Card({ id }) {
+  const cards = useSelector(selectCards);
+  const card = cards[id];
+  const [flipped, setFlipped] = useState(false);
+
+  return (
+    <li>
+      <button className="card" onClick={(e) => setFlipped(!flipped)}>
+        {flipped ? card.back : card.front}
+      </button>
+    </li>
+  );
+}
